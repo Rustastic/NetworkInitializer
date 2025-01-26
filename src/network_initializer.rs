285a65c44cd25cfb9021ceaa5bd27a1e6f230f1e
ilBuso <alessandro.busola@gmail.com>
2025-01-26 11:23:42 +0100

    fixed channels and added doc

--- src/network_initializer.rs
+++ src/network_initializer.rs
@@ -1,3 +1,11 @@
+//! This file contains the Rustastic Network Initializer
+//!
+//! File:   network_initializer.rs
+//!
+//! Brief:  Main file for the Rustastic Network Initializer, containing the necessary step to initialize the drones, simulation controller, server, client
+//!
+//! Author: Alessandro Busola
+
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::{collections::HashMap, fs, thread};
 
@@ -11,6 +19,80 @@ use wg_2024::{
 
 use simulation_controller::SimulationController;
 
+/// Creates a factory function for generating drones of a specified type.
+///
+/// # Type Parameters
+/// * `T` - The type of drone to be created. Must implement the `Drone` trait.
+///
+/// # Returns
+/// * A boxed closure that takes the following arguments and returns a boxed drone:
+///     - `ConfigDrone`: Configuration for the drone.
+///     - `Sender<DroneEvent>`: Sender channel for drone events.
+///     - `HashMap<NodeId, Receiver<DroneCommand>>`: Map of command receiver channels for each node.
+///     - `HashMap<NodeId, Sender<Packet>>`: Map of packet sender channels for each node.
+///     - `HashMap<NodeId, Receiver<Packet>>`: Map of packet receiver channels for each node.
+///
+/// # Panics
+/// * If the required channels for a drone are not found in the provided hashmaps.
+fn drone_factory<T>() -> Box<
+    dyn Fn(
+        &ConfigDrone,
+        &Sender<DroneEvent>,
+        &HashMap<NodeId, Receiver<DroneCommand>>,
+        &HashMap<NodeId, Sender<Packet>>,
+        &HashMap<NodeId, Receiver<Packet>>,
+    ) -> Box<dyn Drone>,
+>
+where
+    T: Drone + 'static,
+{
+    Box::new(
+        |drone, event_send, command_recv_hashmap, packet_send_hashmap, packet_recv_hashmap| {
+            // Get drone's packet receiver channel
+            if let Some(packet_recv) = packet_recv_hashmap.get(&drone.id) {
+                // Create packet send hashmap
+                let mut packet_send = HashMap::<NodeId, Sender<Packet>>::new();
+                // Fill hashmap with only neighbor
+                for neighbor in &drone.connected_node_ids {
+                    packet_send_hashmap
+                        .iter()
+                        .filter(|(node_id, _)| *node_id == neighbor)
+                        .for_each(|(node_id, channel)| {
+                            packet_send.insert(*node_id, channel.clone());
+                        });
+                }
+
+                // Get drone's command receiver channel
+                if let Some(command_recv) = command_recv_hashmap.get(&drone.id) {
+                    return Box::new(T::new(
+                        drone.id,
+                        event_send.clone(),
+                        command_recv.clone(),
+                        packet_recv.clone(),
+                        packet_send,
+                        drone.pdr,
+                    ));
+                } else {
+                    panic!("Command receiver not found for drone {}", drone.id);
+                }
+            } else {
+                panic!("Packet receiver not found for drone {}", drone.id);
+            }
+        },
+    )
+}
+
+/// Opens a configuration file and parses it into a `Config` object.
+///
+/// # Arguments
+/// * `path` - A string slice representing the path to the configuration file.
+///
+/// # Returns
+/// * `Config` - The parsed configuration object.
+///
+/// # Panics
+/// * If the file cannot be read.
+/// * If the TOML data cannot be parsed.
 fn open(path: &str) -> Config {
     // Read content of file src/config.toml
     let config_data = fs::read_to_string(path).expect("Unable to read config file");
@@ -18,101 +100,96 @@ fn open(path: &str) -> Config {
     toml::from_str(&config_data).expect("Unable to parse TOML")
 }
 
-fn create_factory<D>() -> Box<
-    dyn Fn(
-        &ConfigDrone,
-        &Sender<DroneEvent>,
-        &Receiver<DroneCommand>,
-        &HashMap<NodeId, Sender<Packet>>,
-        &HashMap<NodeId, Receiver<Packet>>,
-    ) -> Box<dyn Drone>,
->
-where
-    D: Drone + 'static,
-{
-    Box::new(
-        |drone, event_send, command_recv, all_packet_send, all_packet_recv| {
-            let packet_recv = all_packet_recv.get(&drone.id).unwrap().clone();
-
-            let mut packet_send = HashMap::<NodeId, Sender<Packet>>::new();
-
-            for neighbor in &drone.connected_node_ids {
-                all_packet_send
-                    .iter()
-                    .filter(|(node_id, _)| *node_id == neighbor)
-                    .for_each(|(node_id, channel)| {
-                        packet_send.insert(*node_id, channel.clone());
-                    });
-            }
-
-            Box::new(D::new(
-                drone.id,
-                event_send.clone(),
-                command_recv.clone(),
-                packet_recv,
-                packet_send,
-                drone.pdr,
-            ))
-        },
-    )
-}
-
+/// Initializes and runs the drone simulation.
+///
+/// This function performs the following steps:
+/// 1. Opens and parses the configuration file.
+/// 2. Creates communication channels for events, packets, and commands.
+/// 3. Instantiates drones using factory functions.
+/// 4. Creates a simulation controller and initializes it with drones and neighbors.
+/// 5. Runs the simulation controller and drones on separate threads.
+/// 6. Joins all threads to ensure proper execution.
+///
+/// # Panics
+/// * If the configuration contains fewer than 10 drones.
+/// * If required channels or factories are missing during drone initialization.
 pub fn run() {
     // Open and read File
     let config = open("src/config.toml");
-
     if config.drone.len() < 10 {
         panic!("ERROR: At least 10 drones are required");
     }
 
-    // Simulation controller channels
-    let (command_send, command_recv) = unbounded::<DroneCommand>();
+    // Event channels
     let (event_send, event_recv) = unbounded::<DroneEvent>();
 
-    // HashMap for packet channels
-    let mut all_packet_send = HashMap::<NodeId, Sender<Packet>>::new();
-    let mut all_packet_recv = HashMap::<NodeId, Receiver<Packet>>::new();
+    // Packet channels
+    let mut packet_send = HashMap::<NodeId, Sender<Packet>>::new();
+    let mut packet_recv = HashMap::<NodeId, Receiver<Packet>>::new();
 
+    // Command channels
+    let mut command_send = HashMap::<NodeId, Sender<DroneCommand>>::new();
+    let mut command_recv = HashMap::<NodeId, Receiver<DroneCommand>>::new();
+
+    // Create channels
     for drone in &config.drone {
+        let id = drone.id;
+
+        // Packet channel
         let (pkt_send, pkt_recv) = unbounded::<Packet>();
-        all_packet_send.insert(drone.id, pkt_send);
-        all_packet_recv.insert(drone.id, pkt_recv);
+        packet_send.insert(id, pkt_send);
+        packet_recv.insert(id, pkt_recv);
+
+        // Command channel
+        let (cmd_send, cmd_recv) = unbounded::<DroneCommand>();
+        command_send.insert(id, cmd_send);
+        command_recv.insert(id, cmd_recv);
     }
 
-    // Vector of drones and hashmap for the simulation controller
+    // Vector of drones
     let mut drones: Vec<Box<dyn Drone>> = Vec::new();
+
+    // Hashmap of sender channel of drones
     let mut drones_hashmap = HashMap::<NodeId, (Sender<DroneCommand>, Sender<Packet>)>::new();
 
-    // Factory mapping (uses a common helper function)
+    // Create vector containing all the drones' function
     let drone_factories = vec![
-        create_factory::<rusty_drones::RustyDrone>(),
-        create_factory::<LeDron_James::Drone>(),
-        create_factory::<dr_ones::Drone>(),
-        create_factory::<skylink::SkyLinkDrone>(),
-        create_factory::<rustbusters_drone::RustBustersDrone>(),
-        create_factory::<rust_roveri::RustRoveri>(),
-        create_factory::<rust_do_it::RustDoIt>(),
-        create_factory::<wg_2024_rust::drone::RustDrone>(),
-        create_factory::<null_pointer_drone::MyDrone>(),
-        create_factory::<lockheedrustin_drone::LockheedRustin>(),
+        drone_factory::<rusty_drones::RustyDrone>(),
+        drone_factory::<LeDron_James::Drone>(),
+        drone_factory::<dr_ones::Drone>(),
+        drone_factory::<skylink::SkyLinkDrone>(),
+        drone_factory::<rustbusters_drone::RustBustersDrone>(),
+        drone_factory::<rust_roveri::RustRoveri>(),
+        drone_factory::<rust_do_it::RustDoIt>(),
+        drone_factory::<wg_2024_rust::drone::RustDrone>(),
+        drone_factory::<null_pointer_drone::MyDrone>(),
+        drone_factory::<null_pointer_drone::MyDrone>(),
+        //create_factory::<lockheedrustin_drone::LockheedRustin>(),
     ];
 
     // Generate drones using factories
     for (n, drone) in config.drone.iter().enumerate() {
+        // Get right function
         if let Some(factory) = drone_factories.get(n) {
+            // create drone
             let new_drone = factory(
                 drone,
                 &event_send,
                 &command_recv,
-                &all_packet_send,
-                &all_packet_recv,
+                &packet_send,
+                &packet_recv,
             );
 
             // Add the new drone to the list and hashmap
             drones.push(new_drone);
 
-            if let Some(pkt_send) = all_packet_send.get(&drone.id) {
-                drones_hashmap.insert(drone.id, (command_send.clone(), pkt_send.clone()));
+            // Fill drone Hashmap
+            if let Some(pkt_send) = packet_send.get(&drone.id) {
+                if let Some(cmd_send) = command_send.get(&drone.id) {
+                    drones_hashmap.insert(drone.id, (cmd_send.clone(), pkt_send.clone()));
+                } else {
+                    panic!("Command sender not found for drone {}", drone.id);
+                }
             } else {
                 panic!("Packet sender not found for drone {}", drone.id);
             }
@@ -121,15 +198,24 @@ pub fn run() {
         }
     }
 
-    // Create and initialize simulation controller
-    let mut simulation_controller = SimulationController::new(drones_hashmap, event_recv);
+    // Create an hashmap containing neighbors
+    let mut neighbor = HashMap::<NodeId, Vec<NodeId>>::new();
 
+    // Fill the hashmap
+    for drone in &config.drone {
+        neighbor.insert(drone.id, drone.connected_node_ids.clone());
+    }
+
+    // Create and initialize the Simulation Controller
+    let mut simulation_controller = SimulationController::new(drones_hashmap, event_recv, neighbor);
+
+    // Run simulation controller on different tread
     let controller_handle = thread::spawn(move || {
         simulation_controller.run();
     });
 
     let mut drone_handles = Vec::new();
-    // Run drones
+    // Run drones on different threads
     for mut drone in drones.into_iter() {
         let handle = thread::spawn(move || {
             drone.run();
@@ -137,6 +223,7 @@ pub fn run() {
         drone_handles.push(handle);
     }
 
+    // Join all threads
     controller_handle.join().unwrap();
 
     for handle in drone_handles {
