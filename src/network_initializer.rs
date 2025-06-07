45aef4effe96a259da0d1a07166ef93191eec678
ilBuso <alessandro.busola@gmail.com>
2025-06-07 16:37:34 +0200

    removed skylink

--- src/network_initializer.rs
+++ src/network_initializer.rs
@@ -232,7 +232,7 @@ pub fn run() {
 
     // Create vector containing all the drones' function
     let drone_factories = [
-        /*drone_factory::<rusty_drones::RustyDrone>(),
+        drone_factory::<rusty_drones::RustyDrone>(),
         drone_factory::<LeDron_James::Drone>(),
         drone_factory::<dr_ones::Drone>(),
         //drone_factory::<skylink::SkyLinkDrone>(),
@@ -242,17 +242,7 @@ pub fn run() {
         drone_factory::<rust_do_it::RustDoIt>(),
         drone_factory::<wg_2024_rust::drone::RustDrone>(),
         drone_factory::<null_pointer_drone::MyDrone>(),
-        drone_factory::<lockheedrustin_drone::LockheedRustin>()*/
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
-        drone_factory::<rustbusters_drone::RustBustersDrone>(),
+        drone_factory::<lockheedrustin_drone::LockheedRustin>()
     ];
 
     info!("[ {} ] Creating Drones", "Network Initializer".green());
